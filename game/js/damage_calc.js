5d507e19bef3ea5b4068ab003b22ac63e4b0fd69
原口和大 <kazu@haraguchikazuhiro-no-MacBook-Air.local>
2017-03-24 06:33:19 +0900

    属性ダメージ計算と斬れ味メニューを追加

--- game/js/damage_calc.js
+++ game/js/damage_calc.js
@@ -1,59 +1,14 @@
-// 物理ダメージ計算
-function phys_dmg_calc(atk, type_coef, affi, phys_sharp, phys_weak){
-    console.log(atk);
-    console.log(type_coef);
-    console.log(affi);
-    console.log(phys_sharp);
-    console.log(phys_weak);
+//ダメージ計算ページのjs
+//jsのtextには''を使う。html内は""を使う。
 
-    // 丸め
-    phys_dmg_pure = (atk / (type_coef /100.0)) * (1.0 + 0.25 * affi / 100.0) * phys_sharp * (phys_weak / 100);
-    return Math.round(phys_dmg_pure * 100) / 100;
-}
+//
+// Constants
+//
 
-// 属性ダメージ計算
-function ele_dmg_calc(ele, ele_sharp, ele_weak){
-    return (ele / 10) * ele_sharp * ele_weak;
-}
-
-$(function(){
-    card = '<form class="card">' +
-           '<input id="weapon_name" type="text" value="武器名">' + 
-           '<select id="weapon_type">' +
-                '<option value=480>大剣</option>' +
-                '<option value=330>太刀</option>' +
-                '<option value=140>片手剣</option>' +
-                '<option value=140>双剣</option>' +
-                '<option value=520>ハンマー</option>' +
-                '<option value=520>狩猟笛</option>' +
-                '<option value=540>スラッシュアックス</option>' +
-                '<option value=360>チャージアックス</option>' +
-                '<option value=230>ランス</option>' +
-                '<option value=230>ガンランス</option>' +
-                '<option value=130>ライトボウガン</option>' +
-                '<option value=150>ヘヴィボウガン</option>' +
-                '<option value=120>弓</option>' +
-            '</select>' +
-           // 物理ダメージテーブル
-           '<table>' +
-           '<thead>' +
-           // テーブルヘッド 
-           '<tr>' +
-           '<th>表示力</th>' +
-           '<th>会心率</th>' + 
-           '<th>斬れ味</th>' +
-           '<th>物理肉質</th>' + 
-           '<th>期待ダメージ</th>' +
-           '</tr>' +
-           '</thead>' + 
-           // テーブルボディ
-           '<tbody>' +
-           '<tr>' + 
-           '<td><input id="attack" type="text" value=1000 ></td>' +
-           '<td><input id="affinity" type="text" value=0 ></td>' +
-           '<td id="phys_sharp">' +
-           '<ul id="sharpness_menu">' +
-           '<li>' + 
+//斬れ味メニューのhtml
+const SHARPNESS_MENU = 
+    '<ul id="sharpness_menu">' + 
+        '<li>' +
             '<ul class="child">' +
                 '<li class="red" style="background-color:#FF0000"></li>' +
                 '<li class="orange" style="background-color:#FF9900"></li>' +
@@ -62,82 +17,91 @@ $(function(){
                 '<li class="blue" style="background-color:#4488FF"></li>' +
                 '<li class="white" style="background-color:#FFFFFF"></li>' +
                 '<li class="purple" style="background-color:#CC44FF"></li>' +
-            '</ul>' +
-           '</li>' +
-           '</ul>' + 
-           '</td>' +
-           '<td><input id="phys_weak" value=100 ></td>' +
-           '<td id="phys_dmg"></td>' +
-           '</tr>' +
-           '</tbody>' +
-           '</table>' +
-           //属性ダメージテーブル
-           '<table class="element">' +
-           '<thead>' +
-           // テーブルヘッド 
-           '<tr>' +
-           '<th>属性値</th>' +
-           '<th>斬れ味</th>' +
-           '<th>属性肉質</th>' + 
-           '<th>属性ダメージ</th>' +
-           '</tr>' +
-           '</thead>' + 
-           // テーブルボディ
-           '<tbody>' +
-           '<tr>' + 
-           '<td><input id="element" type="text" value=0 ></td>' +
-           '<td><input id="ele_sharp" type="text"></td>' +
-           '<td><input id="ele_weak" type="text" value=1 ></td>' +
-           '<td id="ele_dmg"></td>' +
-           '</tr>' +
-           '</tbody>' +
-           '</table>' +
-           '</form>';
-    $(card).appendTo('main');
+            '</ul>' + 
+        '</li>' + 
+    '</ul>';
 
-    //ドロップダウンメニュー
+const DROPDOWN_TRIGGER = '<span class="dropdown_trigger"></span>';
+
+//斬れ味の色と補正値のマップ
+//物理補正値
+const PHYS_SHARP_DICT = {
+    'rgb(255, 0, 0)'    : 0.5, //赤
+    'rgb(255, 153, 0)'  : 0.75, //橙
+    'rgb(255, 255, 0)'  : 1.0, //黄
+    'rgb(0, 255, 0)'    : 1.05, //緑
+    'rgb(68, 136, 255)' : 1.2, //青
+    'rgb(255, 255, 255)': 1.32, //白
+    'rgb(204, 68, 255)' : 1.45 //紫
+};
+
+//属性補正値
+const ELE_SHARP_DICT = {
+    'rgb(255, 0, 0)'    : 0.25, //赤
+    'rgb(255, 153, 0)'  : 0.5, //橙
+    'rgb(255, 255, 0)'  : 0.75, //黄
+    'rgb(0, 255, 0)'    : 1.0, //緑
+    'rgb(68, 136, 255)' : 1.0625, //青
+    'rgb(255, 255, 255)': 1.125, //白
+    'rgb(204, 68, 255)' : 1.2 //紫
+};
+
+
+//
+// Functions
+//
+
+// 物理ダメージ計算
+function phys_dmg_calc(atk, type_coef, affi, phys_sharp, phys_weak){
+    return (atk / (type_coef /100.0)) * (1.0 + 0.25 * affi / 100.0) *          phys_sharp * (phys_weak / 100);
+}
+
+// 属性ダメージ計算
+function ele_dmg_calc(ele, ele_sharp, ele_weak){
+    return (ele / 10) * ele_sharp * (ele_weak / 100);
+}
+
+//小数点第２位までに丸める
+function to_2_decimal_places(n){
+    return Math.round(n * 100) / 100;
+}
+
+$(function(){
+    card = $('.card')
+
+    //斬れ味ドロップダウンメニューの追加
+    $("#phys_sharp").append(SHARPNESS_MENU);
+    $("#ele_sharp").append(SHARPNESS_MENU);
+
+    //ドロップダウンメニューの動きを設定。
     $('#sharpness_menu li').hover(function(){
         $('ul:not(:animated)', this).slideDown();
     }, function(){
         $('ul.child', this).slideUp();
     });
 
-    //斬れ味の色と補正値のマップ
-    phys_sharp_dict = {
-        'rgb(255, 0, 0)': 0.5, //赤
-        'rgb(255, 153, 0)': 0.75, //橙
-        'rgb(255, 255, 0)': 1.00, //黄
-        'rgb(0, 255, 0)': 1.05, //緑
-        'rgb(68, 136, 255)': 1.2, //青
-        'rgb(255, 255, 255)': 1.32, //白
-        'rgb(204, 68, 255)': 1.44 //紫
-    };
-
     // 切れ味のドロップダウンリストをクリックすると、
     // <td pyhs_sharp>の背景色が変更される。
     $('.child li').on('click', function(){
         $('#phys_sharp').css('background-color', $(this).css('background-color'));
+        $('#ele_sharp').css('background-color', $(this).css('background-color'));
     });
-
-
 
     $('.card').change(function(){
         //物理ダメージの計算
         phys_dmg = phys_dmg_calc($('#attack').val(),
                                  $('select#weapon_type').val(),
                                  $('#affinity').val(),
-                                 phys_sharp_dict[$('#phys_sharp').css('background-color')],
+                                 PHYS_SHARP_DICT[$('#phys_sharp').css('background-color')],
                                  $('#phys_weak').val());
 
         // 属性ダメージの計算
         ele_dmg = ele_dmg_calc($('#element').val(), 
-                               $('#ele_sharp').val(),
+                               ELE_SHARP_DICT[$('#ele_sharp').css('background-color')],
                                $('#ele_weak').val());
-        console.log('よばれた。');
-        $('#phys_dmg').text(phys_dmg);
-        $('#ele_dmg').text(ele_dmg);
+        $('#phys_dmg').text(to_2_decimal_places(phys_dmg));
+        $('#ele_dmg').text(to_2_decimal_places(ele_dmg));
     });
-
 });
 
 
