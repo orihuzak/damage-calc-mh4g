0d702f4f186badb2ee309cf3e8b646accafd6353
原口和大 <kazu@haraguchikazuhiro-no-MacBook-Air.local>
2017-04-01 08:21:10 +0900

    武器係数マップを追加。ゲーム側の仕様にならい、物理ダメと属性ダメを加算後に小数点以下を切り捨てるようにした。

--- game/js/damage_calc.js
+++ game/js/damage_calc.js
@@ -5,25 +5,19 @@
 // Constants
 //
 
-//斬れ味メニューのhtml
-// *class="色"部分はいるのか？
-const SHARPNESS_MENU = 
-    '<ul class="sharpness_menu">' + 
-        '<li>' +
-            '<ul class="child">' +
-                '<li style="background-color:#FF0000"></li>' +
-                '<li style="background-color:#FF9900"></li>' +
-                '<li style="background-color:#FFFF00"></li>' +
-                '<li style="background-color:#00FF00"></li>' +
-                '<li style="background-color:#4488FF"></li>' +
-                '<li style="background-color:#FFFFFF"></li>' +
-                '<li style="background-color:#CC44FF"></li>' +
-            '</ul>' + 
-        '</li>' + 
-    '</ul>';
-
-//ドロップダウンメニューだとわかりやすくするためのトリガー用のhtml
-const MENU_TRIGGER = '<span class="trigger_panel"><span class="trigger"></span></span>';
+//武器種と武器係数
+const WEAPON_TYPES = {
+    '大剣': 4.8,
+    '太刀': 3.3,
+    '片手剣': 1.4,
+    '双剣': 1.4,
+    'ハンマー': 5.2,
+    '狩猟笛': 5.2,
+    'スラッシュアックス': 5.4,
+    'チャージアックス': 3.6,
+    'ランス': 2.3,
+    'ガンランス': 2.3
+};
 
 //斬れ味の色と補正値のマップ
 //物理補正値
@@ -48,6 +42,26 @@ const ELE_SHARP_DICT = {
     'rgb(204, 68, 255)' : 1.2 //紫
 };
 
+//斬れ味メニューのhtml
+const SHARPNESS_MENU = 
+    '<ul class="sharpness_menu">' + 
+        '<li>' +
+            '<ul class="child">' +
+                '<li style="background-color:#FF0000"></li>' +
+                '<li style="background-color:#FF9900"></li>' +
+                '<li style="background-color:#FFFF00"></li>' +
+                '<li style="background-color:#00FF00"></li>' +
+                '<li style="background-color:#4488FF"></li>' +
+                '<li style="background-color:#FFFFFF"></li>' +
+                '<li style="background-color:#CC44FF"></li>' +
+            '</ul>' + 
+        '</li>' + 
+    '</ul>';
+
+//ドロップダウンメニューだとわかりやすくするためのトリガー用のhtml
+const MENU_TRIGGER = '<span class="trigger_panel"><span class="trigger"></span></span>';
+
+
 
 //
 // Functions
@@ -59,11 +73,13 @@ function to_2_decimal_places(n){
 }
 
 // 物理ダメージ計算
+// (攻撃力/武器係数) * 会心期待値 * 斬れ味 * (物理肉質/100)
 function phys_dmg_calc(atk, type_coef, affi, phys_sharp, phys_weak){
-    return (atk / (type_coef /100.0)) * (1.0 + 0.25 * affi / 100.0) *          phys_sharp * (phys_weak / 100);
+    return (atk / type_coef) * (1.0 + 0.25 * affi / 100.0) *          phys_sharp * (phys_weak / 100);
 }
 
 // 属性ダメージ計算
+// (属性値/10) * 斬れ味 * (属性肉質/100)
 function ele_dmg_calc(ele, ele_sharp, ele_weak){
     return (ele / 10) * ele_sharp * (ele_weak / 100);
 }
@@ -94,25 +110,32 @@ $(function(){
     });
 
     // 切れ味のドロップダウンリストをクリックすると、
-    // その<td>の背景色が変更される。
+    // ul.sharpness_menuの背景色が変更される。
     $('.child li').on('click', function(){
         $('.sharpness_menu').css('background-color', $(this).css('background-color'));
     });
 
-    $('.card').change(function(){
+    // 計算と出力
+    $('.card').on('click blur keydown keyup keypress change', function(){
         //物理ダメージの計算
-        phys_dmg = phys_dmg_calc($('#attack').val(),
-                                 $('select.weapon_types').val(),
-                                 $('#affinity').val(),
-                                 PHYS_SHARP_DICT[$('.sharpness').css('background-color')],
-                                 $('#phys_weak').val());
+        phys_dmg = phys_dmg_calc($('.attack').val(),
+                                 WEAPON_TYPES[$('select.weapon_types option:selected').text()],
+                                 $('.affinity').val(),
+                                 PHYS_SHARP_DICT[$('.sharpness_menu').css('background-color')],
+                                 $('.phys_weak').val());
 
         // 属性ダメージの計算
-        ele_dmg = ele_dmg_calc($('#element').val(), 
-                               ELE_SHARP_DICT[$('.sharpness').css('background-color')],
-                               $('#ele_weak').val());
-        $('#phys_dmg').text(to_2_decimal_places(phys_dmg));
-        $('#ele_dmg').text(to_2_decimal_places(ele_dmg));
+        ele_dmg = ele_dmg_calc($('.element').val(), 
+                               ELE_SHARP_DICT[$('.sharpness_menu').css('background-color')],
+                               $('.ele_weak').val());
+        
+        //計算結果の出力
+        // 物理と属性ダメは、表示ために小数点第2位までで四捨五入
+        $('.phys_dmg').text(to_2_decimal_places(phys_dmg));
+        $('.ele_dmg').text(to_2_decimal_places(ele_dmg));
+
+        //物理ダメと属性ダメを合算後に小数点以下を切り捨て
+        $('.sum_dmg').text(Math.floor(phys_dmg + ele_dmg));
     });
 });
 
